a3097647b781d300e78ab28a3dccb21a74f1726f
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-07-24 16:55:25 -0500

    Fixed up installing files

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cd `dirname $0`
+
 mvn install:install-file -Dfile=galaxybootstrap-0.4.0-SNAPSHOT.jar -DpomFile=galaxybootstrap-0.4.0-SNAPSHOT.pom -DcreateChecksum=true
 mvn install:install-file -Dfile=blend4j-0.2.0-SNAPSHOT.jar -DpomFile=blend4j-0.2.0-SNAPSHOT.pom -DcreateChecksum=true
