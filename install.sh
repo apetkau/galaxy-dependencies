264e8cbc55fab4dcb05981ee84752b32ed21ab0a
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-10-14 13:21:38 -0500

    Upgraded blend4j and removed the need for galaxy-bootstrap dependencies

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`
 
-mvn install:install-file -Dfile=galaxybootstrap-0.4.0-SNAPSHOT.jar -DpomFile=galaxybootstrap-0.4.0-SNAPSHOT.pom -DcreateChecksum=true
-mvn install:install-file -Dfile=blend4j-0.2.0-SNAPSHOT.jar -DpomFile=blend4j-0.2.0-SNAPSHOT.pom -DcreateChecksum=true
+# Git commit 1167ef08bbf7359d781e910ce8253fc36295f291
+mvn install:install-file -Dfile=blend4j-0.2.0-SNAPSHOT.jar -DpomFile=blend4j-0.2.0-SNAPSHOT.pom -Dversion=0.2.0-SNAPSHOT-1167ef08bbf7359d781e910ce8253fc36295f291 -DcreateChecksum=true
