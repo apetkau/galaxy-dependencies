c36b642040e28a13e0b61f95ce0531f700aa647e
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-02-03 09:50:12 -0600

    Added better support for tools, from https://github.com/apetkau/blend4j/commit/06fa5da9722cf938306359ea1856a37ac4d429eb

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`
 
-# Git commit 1167ef08bbf7359d781e910ce8253fc36295f291
-mvn install:install-file -Dfile=blend4j-0.2.0-SNAPSHOT.jar -DpomFile=blend4j-0.2.0-SNAPSHOT.pom -Dversion=0.2.0-SNAPSHOT-1167ef08bbf7359d781e910ce8253fc36295f291 -DcreateChecksum=true
+# Git commit 06fa5da9722cf938306359ea1856a37ac4d429eb
+mvn install:install-file -Dfile=blend4j-0.2.0-SNAPSHOT.jar -DpomFile=blend4j-0.2.0-SNAPSHOT.pom -Dversion=0.2.0-SNAPSHOT-06fa5da9722cf938306359ea1856a37ac4d429eb -DcreateChecksum=true
