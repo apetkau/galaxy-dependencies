3ceeb67f4fd427c59bda97e17b4b2d595751e35a
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-04-28 15:46:32 -0500

    updated blend4j https://github.com/apetkau/blend4j/commit/7a4695c6cdaa345e1b31f9c1f495b581e56f6810

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`
 
-# Git commit f1044f9919aea71bd1d089bbf75108ee1d68c086
-mvn install:install-file -Dfile=blend4j-0.2.0-SNAPSHOT.jar -DpomFile=blend4j-0.2.0-SNAPSHOT.pom -Dversion=0.2.0-SNAPSHOT-f1044f9919aea71bd1d089bbf75108ee1d68c086 -DcreateChecksum=true
+# Git commit 7a4695c6cdaa345e1b31f9c1f495b581e56f6810
+mvn install:install-file -Dfile=blend4j-0.2.0-SNAPSHOT.jar -DpomFile=blend4j-0.2.0-SNAPSHOT.pom -Dversion=0.2.0-SNAPSHOT-7a4695c6cdaa345e1b31f9c1f495b581e56f6810 -DcreateChecksum=true
