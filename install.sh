6b4528208942fedef7886118909a00f36224c976
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2016-07-25 12:00:08 -0500

    Updating blend4j to https://github.com/apetkau/blend4j/commit/29f28db211c300fcaf21a7a5992478fc5a9893cd

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 
 cd `dirname $0`
 
-# Git commit ec2499861b11310743862b2d3e2f0a88dde27a51
-mvn install:install-file -Dfile=blend4j-0.2.0-SNAPSHOT.jar -DpomFile=blend4j-0.2.0-SNAPSHOT.pom -Dversion=0.2.0-SNAPSHOT-ec2499861b11310743862b2d3e2f0a88dde27a51 -DcreateChecksum=true
+# Git commit 29f28db211c300fcaf21a7a5992478fc5a9893cd
+mvn install:install-file -Dfile=blend4j-0.2.0-SNAPSHOT.jar -DpomFile=blend4j-0.2.0-SNAPSHOT.pom -Dversion=0.2.0-SNAPSHOT-29f28db211c300fcaf21a7a5992478fc5a9893cd -DcreateChecksum=true
+
+# Git commit 50db3a199f9f55d76842328fd46427f0738a4a42
+mvn install:install-file -Dfile=galaxybootstrap-0.6.1-SNAPSHOT.jar -DpomFile=galaxybootstrap-0.6.1-SNAPSHOT.pom -Dversion=0.6.1-SNAPSHOT-50db3a199f9f55d76842328fd46427f0738a4a42 -DcreateChecksum=true
